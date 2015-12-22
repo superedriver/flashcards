e6de51e88c78f1a319d7f10aa8746ea193188c58
super edriver <superedriver@gmail.com>
2015-12-22 21:21:59 +0200

    Осталось: 1.  Метод Destroy повесить на кнопку Delete 2. Добавить валидации. 3. Заюзать гем simple_form 4. Сделать красивее верстку

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,5 @@
 class CardsController < ApplicationController
+
   def index
     @cards = Card.all
   end
@@ -6,10 +7,54 @@ class CardsController < ApplicationController
   def show
     @card = Card.find_by id: params[:id]
 
-    if @card
-      render "cards/show"
-    else
+    unless @card
       render text: "Page not found", status: 404
     end
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.review_date = DateTime.now.to_date + 3.days
+    @card.save
+
+    if @card.errors.empty?
+      redirect_to card_path(@card)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @card.update_attributes(card_params)
+    @card.review_date = DateTime.now.to_date + 3.days
+    @card.save
+
+    if @card.errors.empty?
+      redirect_to card_path(@card)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to action: "index"
+  end
+
+
+  private
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
+  end
+
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,4 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
 end
