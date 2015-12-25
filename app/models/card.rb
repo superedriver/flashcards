af5fef94b4cfbb2f208c876380396fad0429d765
super edriver <superedriver@gmail.com>
2015-12-26 00:11:55 +0200

    The third task is ready

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,11 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
 
-  before_save :original_and_translated_texts_are_different
+  validate :check_difference
 
   private
-    def original_and_translated_texts_are_different
-      self.original_text.downcase != self.translated_text.downcase
+    def check_difference
+      self.errors.add(:difference, "can't be greater than total value") if
+          self.original_text.downcase == self.translated_text.downcase
     end
 end
