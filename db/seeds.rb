9c76163ec2781b8ded6a2bf442a2a9107bb85535
super edriver <superedriver@gmail.com>
2015-12-29 23:58:59 +0200

    Сделал 4-е задание

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'open-uri'
+
+page = Nokogiri::HTML(open("http://www.lesson1.ru/vocabulary/lexicographer/food.html"))
+trs = page.css('table.tbl3 tr:not(.first)')
+
+trs.each do |tr|
+  tds = tr.css("td")
+  Card.create( original_text: tds[0].content, translated_text: tds[1].content )
+end
