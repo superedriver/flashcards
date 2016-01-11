8893d43b9d0790044b61547f6f3cd94c57aeed67
super edriver <superedriver@gmail.com>
2016-01-11 18:50:55 +0200

    Changed some tests

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,26 +1,26 @@
 require "rails_helper"
 
-RSpec.describe Card, :type => :model do
+RSpec.describe Card, type: :model do
 
-  describe "check_translation? group" do
+  describe "#check_translation?" do
     before(:each) do
       @card = FactoryGirl.create(:card)
     end
 
     it "Correct translation" do
-      expect(@card.check_translation?("мяч")).to eq(true)
+      expect(@card.check_translation?("мяч")).to be true
     end
 
     it "Incorrect translation" do
-      expect(@card.check_translation?("мяч1")).to eq(false)
+      expect(@card.check_translation?("мяч1")).to be false
     end
 
     it "Correct translation upcase" do
-      expect(@card.check_translation?("МЯЧ")).to eq(true)
+      expect(@card.check_translation?("МЯЧ")).to be true
     end
   end
 
-  describe "review date group" do
+  describe "#review date" do
     before(:each) do
       @card = FactoryGirl.create(:card)
     end
@@ -31,20 +31,15 @@ RSpec.describe Card, :type => :model do
 
     it "change_review_date!" do
       @card.update_column(:review_date, Date.current - 2.day)
-      @card.change_review_date!
-      expect(@card.review_date).to eq(Date.current + 3.days)
+      expect {
+        @card.change_review_date!
+      }.to change { @card.review_date }.to(Date.current + 3.days)
     end
   end
 
   describe "check_difference validation" do
-    # it "valid?" do
-    #   expect(Card.create(original_text: "мяч", translated_text: "мяч")).to be_invalid
-    # end
-
-    it "test size" do
-      @card = Card.new(original_text: "мяч", translated_text: "мяч")
-      @card.valid?
-      expect(@card.errors.size).to eq(1)
+    it "valid?" do
+      expect(Card.create(original_text: "мяч", translated_text: "мяч")).to be_invalid
     end
 
     it "the same values" do
