5a4b56fe16975b4e6c5a8dacdf5d79adccb15edf
super edriver <superedriver@gmail.com>
2016-01-12 19:26:13 +0200

    Correted  tests

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -13,11 +13,16 @@ describe "check_root_path", type: :feature do
     expect(page).to have_content I18n.t('activerecord.attributes.card.translated_text')
   end
 
-  it "fill the field" do
-    visit root_path
+  it "incorrect value" do
     fill_in :original_text, with: 'qwerty'
     click_button I18n.t('buttons.check')
     expect(page).to have_content I18n.t('compare_result.not_right').mb_chars.upcase
+  end
+
+  it "correct value" do
+    fill_in :original_text, with: 'мяч'
+    click_button I18n.t('buttons.check')
+    expect(page).to have_content I18n.t('compare_result.right').mb_chars.upcase
   end
 end
 
