589ae96d050b2c9aff483fe960456238777dc3eb
super edriver <superedriver@gmail.com>
2016-01-19 18:54:07 +0200

    Added validation association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_many :cards, dependent: :destroy
+  # validates_associated :cards
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :user do
     email "MyString"
-password "MyString"
+    password "MyString"
   end
-
 end
