37ceb447c35c7ce42c22eab3ed2878d1ebd135ab
super edriver <superedriver@gmail.com>
2016-06-04 20:43:49 +0300

    added CardMailer specs

--- spec/mailers/cards_mailer_spec.rb
+++ spec/mailers/cards_mailer_spec.rb
@@ -1,5 +1,29 @@
 require "rails_helper"
 
 RSpec.describe CardsMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".pending_cards" do
+
+    before do
+      @user = create(:user)
+      @mail = CardsMailer.pending_cards(@user)
+    end
+
+    describe "headers" do
+      it "renders the subject" do
+        expect(@mail.subject).to eq("Hi! You have active cards.")
+      end
+
+      it "renders the sender email" do
+        expect(@mail.from).to eq(["noreply@flashcards.com"])
+      end
+
+      it "renders the receiver email" do
+        expect(@mail.to).to eq([@user.email])
+      end
+    end
+
+    it "user.name in the letter" do
+      expect(@mail.body.encoded).to match(@user.email)
+    end
+  end
 end
