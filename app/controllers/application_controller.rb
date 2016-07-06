15f9af001c1266e271d236b149bc8e41e0afaffb
super edriver <superedriver@gmail.com>
2016-07-06 23:25:24 +0300

    added default locale while saving user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,29 +6,28 @@ class ApplicationController < ActionController::Base
   before_action :require_login
   before_filter :set_locale
 
-  def set_locale
-    locale = if current_user
-               current_user.locale
-             elsif params[:locale]
-               session[:locale] = params[:locale]
-             elsif session[:locale]
-               session[:locale]
-             else
-               http_accept_language.compatible_language_from(I18n.available_locales)
-             end
+  private
+    def set_locale
+      locale = if current_user
+                 current_user.locale
+               elsif params[:locale]
+                 session[:locale] = params[:locale]
+               elsif session[:locale]
+                 session[:locale]
+               else
+                 http_accept_language.compatible_language_from(I18n.available_locales)
+               end
 
-    if locale && I18n.available_locales.include?(locale.to_sym)
-      session[:locale] = I18n.locale = locale.to_sym
+      if locale && I18n.available_locales.include?(locale.to_sym)
+        session[:locale] = I18n.locale = locale.to_sym
+      end
     end
-  end
-
-  protected
 
     def show_errors(exception)
       render exception, status: 404
     end
 
-  private
+
 
     def not_authenticated
       redirect_to login_path, notice: I18n.t('flashes.login.login_first')
