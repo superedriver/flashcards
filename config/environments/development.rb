d478afd8072079a9452361b0188d4c57c9df4358
superedriver <superedriver@gmail.com>
2016-10-07 15:47:06 +0300

    Fixed Hound messages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,13 +58,12 @@ Rails.application.configure do
 
   # Mailgun
   config.action_mailer.smtp_settings = {
-      port:            587,
-      address:         'smtp.mailgun.org',
-      user_name:       ENV['MAILGUN_USERNAME'],
-      password:        ENV['MAILGUN_PASSWORD'],
-      domain:          ENV['MAILGUN_DOMAIN'],
-      authentication:  :plain
+    port:            587,
+    address:         "smtp.mailgun.org",
+    user_name:       ENV["MAILGUN_USERNAME"],
+    password:        ENV["MAILGUN_PASSWORD"],
+    domain:          ENV["MAILGUN_DOMAIN"],
+    authentication:  :plain
   }
   config.action_mailer.delivery_method = :smtp
-
 end
