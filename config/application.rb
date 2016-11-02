890831d9dc9421bb85cee21562c882f3dc1d87c1
superedriver <superedriver@gmail.com>
2016-11-02 10:37:54 +0200

    Configured codeclimate

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Flashcards
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Kyiv'
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
 
     config.i18n.available_locales = [:en, :ru, :ua]
