9b9bc30bd5a6b975d22c2347a31fa4546b91370a
superedriver <superedriver@gmail.com>
2016-11-08 14:25:23 +0200

    set  up nginx1

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,9 @@ threads 1, 6
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
+preload_app!
 # Default to production
+port        ENV['PORT']     || 3000
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
