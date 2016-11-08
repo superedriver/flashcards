04e2b511203930febaca21a9b4d3fd92da21438c
superedriver <superedriver@gmail.com>
2016-11-08 14:35:10 +0200

    set  up nginx1

--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,49 @@
-# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-# threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-# threads threads_count, threads_count
+# # workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# # threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+# # threads threads_count, threads_count
+# #
+# # preload_app!
+# #
+# # rackup      DefaultRackup
+# # port        ENV['PORT']     || 3000
+# # environment ENV['RACK_ENV'] || 'development'
+# #
+# # on_worker_boot do
+# #   # Worker specific setup for Rails 4.1+
+# #   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+# #   ActiveRecord::Base.establish_connection
+# # end
+#
+# # Change to match your CPU core count
+# workers 2
+#
+# # Min and Max threads per worker
+# threads 1, 6
+#
+# app_dir = File.expand_path("../..", __FILE__)
+# shared_dir = "#{app_dir}/shared"
 #
 # preload_app!
-#
-# rackup      DefaultRackup
+# # Default to production
 # port        ENV['PORT']     || 3000
-# environment ENV['RACK_ENV'] || 'development'
+# rails_env = ENV['RAILS_ENV'] || "production"
+# environment rails_env
+#
+# # Set up socket location
+# bind "unix://#{shared_dir}/sockets/puma.sock"
+#
+# # Logging
+# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+#
+# # Set master PID and state locations
+# pidfile "#{shared_dir}/pids/puma.pid"
+# state_path "#{shared_dir}/pids/puma.state"
+# activate_control_app
 #
 # on_worker_boot do
-#   # Worker specific setup for Rails 4.1+
-#   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-#   ActiveRecord::Base.establish_connection
+#   require "active_record"
+#   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+#   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 # end
 
 # Change to match your CPU core count
@@ -23,9 +55,7 @@ threads 1, 6
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
-preload_app!
 # Default to production
-port        ENV['PORT']     || 3000
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
