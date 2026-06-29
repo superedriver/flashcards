4c26982bc168bb3e3e1d4f7a59a92aa4d50930de
superedriver <superedriver@gmail.com>
2026-06-29 15:44:01 +0200

    TASK-04.04 Add Prisma profile repository

--- apps/api/src/modules/account/account.module.ts
+++ apps/api/src/modules/account/account.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { USER_PROFILE_REPOSITORY } from './application/ports/user-profile-repository.port';
+import { PrismaUserProfileRepository } from './infrastructure/persistence/prisma-user-profile.repository';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: USER_PROFILE_REPOSITORY,
+      useClass: PrismaUserProfileRepository,
+    },
+  ],
+  exports: [USER_PROFILE_REPOSITORY],
+})
 export class AccountModule {}
