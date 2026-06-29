3eec9a6143b8a7a5dcde8b79e94969799f4a9ed3
superedriver <superedriver@gmail.com>
2026-06-29 16:37:50 +0200

    TASK-04.10 Add updateProfile mutation

--- apps/api/src/modules/account/presentation/graphql/resolvers/account.resolver.ts
+++ apps/api/src/modules/account/presentation/graphql/resolvers/account.resolver.ts
@@ -1,16 +1,22 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthUser } from '../../../../auth/domain/types';
 import { CurrentUser } from '../../../../auth/presentation/graphql/decorators/current-user.decorator';
 import { GqlAuthGuard } from '../../../../auth/presentation/graphql/guards/gql-auth.guard';
 import { UserRole } from '../../../../auth/presentation/graphql/types/user-role.type';
 import { GetMyAccountUseCase } from '../../../application/use-cases/get-my-account.use-case';
+import { UpdateProfileUseCase } from '../../../application/use-cases/update-profile.use-case';
+import { UpdateProfileInput } from '../inputs/update-profile.input';
 import { MyAccountType } from '../types/my-account.type';
 import { ThemePreference } from '../types/theme-preference.type';
+import { UserProfileType } from '../types/user-profile.type';
 
 @Resolver()
 export class AccountResolver {
-  constructor(private readonly getMyAccountUseCase: GetMyAccountUseCase) {}
+  constructor(
+    private readonly getMyAccountUseCase: GetMyAccountUseCase,
+    private readonly updateProfileUseCase: UpdateProfileUseCase,
+  ) {}
 
   @Query(() => MyAccountType)
   @UseGuards(GqlAuthGuard)
@@ -28,5 +34,18 @@ export class AccountResolver {
         themePreference: account.settings.themePreference as ThemePreference,
       },
     };
+  }
+
+  @Mutation(() => UserProfileType)
+  @UseGuards(GqlAuthGuard)
+  async updateProfile(
+    @CurrentUser() user: AuthUser,
+    @Args('input') input: UpdateProfileInput,
+  ): Promise<UserProfileType> {
+    return this.updateProfileUseCase.execute({
+      userId: user.id,
+      displayName: input.displayName,
+      avatarUrl: input.avatarUrl,
+    });
   }
 }
