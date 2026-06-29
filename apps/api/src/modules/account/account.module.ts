9b2cfc0d7e2a990604f275d0e8356586501cfc06
superedriver <superedriver@gmail.com>
2026-06-29 15:51:21 +0200

    TASK-04.06 Add GetMyAccountUseCase

--- apps/api/src/modules/account/account.module.ts
+++ apps/api/src/modules/account/account.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { USER_PROFILE_REPOSITORY } from './application/ports/user-profile-repository.port';
 import { USER_SETTINGS_REPOSITORY } from './application/ports/user-settings-repository.port';
+import { GetMyAccountUseCase } from './application/use-cases/get-my-account.use-case';
 import { PrismaUserProfileRepository } from './infrastructure/persistence/prisma-user-profile.repository';
 import { PrismaUserSettingsRepository } from './infrastructure/persistence/prisma-user-settings.repository';
 
 @Module({
+  imports: [AuthModule],
   providers: [
     {
       provide: USER_PROFILE_REPOSITORY,
@@ -14,7 +17,12 @@ import { PrismaUserSettingsRepository } from './infrastructure/persistence/prism
       provide: USER_SETTINGS_REPOSITORY,
       useClass: PrismaUserSettingsRepository,
     },
+    GetMyAccountUseCase,
   ],
-  exports: [USER_PROFILE_REPOSITORY, USER_SETTINGS_REPOSITORY],
+  exports: [
+    USER_PROFILE_REPOSITORY,
+    USER_SETTINGS_REPOSITORY,
+    GetMyAccountUseCase,
+  ],
 })
 export class AccountModule {}
